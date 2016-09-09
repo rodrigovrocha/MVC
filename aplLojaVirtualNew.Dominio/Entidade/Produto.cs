b0dc68254ca8144f73a94211ee61cb2d54c02341
RodrigoRocha <rodrigo.vrocha@outlook.com>
2016-09-09 18:00:14 -0300

    Entidade

---
+++ aplLojaVirtualNew.Dominio/Entidade/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplLojaVirtualNew.Dominio.Entidade
+{
+    public class Produto
+    {
+        public int produtoId { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public decimal preco { get; set; }
+        public string categoria { get; set; }
+    }
+}
